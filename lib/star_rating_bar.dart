7968e82ae39983777145afa2e341e4211941336a
QUANZ <rbtkd2642@naver.com>
2023-06-12 22:48:10 +0900

    third commit- trying to connect review order api

--- lib/star_rating_bar.dart
+++ lib/star_rating_bar.dart
@@ -33,7 +33,7 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.amber,
-        
+
       ),
       home: Builder(
         builder: (context) => Scaffold(
